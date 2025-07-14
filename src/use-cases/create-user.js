e3d8017f7fc9d1e982bd9086df2bbd42249b2052
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-14 19:58:40 -0300

    feat: add CreateUserController and validations

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,26 +1,26 @@
 import { v4 as uuidv4 } from 'uuid'
 import bcrypt from 'bcrypt'
-import { PostgresCreateUserRepository } from '../repositories/postgres/create-user'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
 
 export class CreateUserUseCase {
     async excute(createUserParams) {
         // TODO: verificar se o e-mail ja esta em uso
         // Gerar ID do usuário
-        const userdId = uuidv4()
+        const userId = uuidv4()
 
         // criptografar a senha
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
         // inserir o usuário o banco de dados
         const user = {
             ...createUserParams,
-            id: userdId,
+            id: userId,
             password: hashedPassword,
         }
 
         // chamar o repositório
         const postgresCreateUserRepository = new PostgresCreateUserRepository()
 
-        const createdUser = await postgresCreateUserRepository.excute(user)
+        const createdUser = await postgresCreateUserRepository.execute(user)
 
         return createdUser
     }
