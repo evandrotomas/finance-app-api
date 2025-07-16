f8c63d45e80ea616587e3b889b6a5a6041fa86ad
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-16 18:38:15 -0300

    feat: add injection in GetUserByIdUseCase and GetUserByIdController

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,4 +1,3 @@
-import { GetUserByIdUseCase } from '../use-cases/index.js'
 import {
     checkIfIdIsValid,
     invalidIdResponse,
@@ -8,6 +7,10 @@ import {
 } from './helpers/index.js'
 
 export class GetUserByIdController {
+    constructor(getUserByIdUseCase) {
+        this.getUserByIdUseCase = getUserByIdUseCase
+    }
+
     async execute(httpRequest) {
         try {
             const userId = httpRequest.params.userId
@@ -17,9 +20,8 @@ export class GetUserByIdController {
             if (!isIdValid) {
                 return invalidIdResponse()
             }
-            const getUserByIdUseCase = new GetUserByIdUseCase()
 
-            const user = await getUserByIdUseCase.execute(
+            const user = await this.getUserByIdUseCase.execute(
                 httpRequest.params.userId,
             )
 
