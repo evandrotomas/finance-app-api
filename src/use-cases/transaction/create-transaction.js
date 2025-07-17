af5542a5a312f3d870ea5be32fdd0348c7edd27a
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-17 19:15:03 -0300

    feat: add CreateTransactionUseCase

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,32 @@
+import { v4 as uuidv4 } from 'uuid'
+import { UserNotFoundError } from '../../errors/user.js'
+
+export class CreateTransactionUseCase {
+    constructor(createTransactionRepository, getUserByIdRepository) {
+        this.createTransactionRepository = createTransactionRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+
+    async execute(createTransactionParams) {
+        // validar se o usuário existe
+        const userId = createTransactionParams.userId
+
+        const user = await this.getUserByIdRepository.execute(userId)
+
+        // se não existir, lançar erro
+        if (!user) {
+            throw new UserNotFoundError(userId)
+        }
+
+        // se existir, criar a ID da transação
+        const transactionId = uuidv4()
+
+        // criar a transação
+        const transaction = await this.createTransactionRepository.execute(
+            ...createTransactionParams,
+            transactionId,
+        )
+
+        return transaction
+    }
+}
