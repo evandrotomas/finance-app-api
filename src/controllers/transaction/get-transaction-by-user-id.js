65798d1d82879e6451a131643db9642483dc8452
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-18 18:33:13 -0300

    feat: add GetTransactionByUserIdController

--- src/controllers/transaction/get-transaction-by-user-id.js
+++ src/controllers/transaction/get-transaction-by-user-id.js
@@ -1,18 +1,48 @@
-// import { serverError } from '../helpers'
+import { UserNotFoundError } from '../../errors/user.js'
+import {
+    requiredFieldsIsMissingResponse,
+    serverError,
+    userNotFoundResponse,
+} from '../helpers.sj'
+import { ok } from '../helpers/http.js'
+import { checkIfIdIsValid, invalidIdResponse } from '../helpers/validation.js'
 
 export class GetTransactionByUserIdController {
     constructor(getTransactionsByUserIdUseCase) {
         this.getTransactionsByUserIdUseCase = getTransactionsByUserIdUseCase
     }
 
-    // async execute(httpRequest) {
-    //     try {
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.query.userId
+            // verificar se o userId foi passado como parametro
+            if (!userId) {
+                return requiredFieldsIsMissingResponse('userId')
+            }
 
-    //     } catch (error) {
-    //         console.error(error)
-    //         return serverError({
-    //             message: 'An error occurred while get the transaction',
-    //         })
-    //     }
-    // }
+            // verificar se o userId é um ID válido
+            const userIdIsValid = checkIfIdIsValid(userId)
+
+            if (!userIdIsValid) {
+                return invalidIdResponse()
+            }
+
+            // chamar o use case
+            const transactions =
+                await this.getTransactionsByUserIdUseCase.execute({
+                    userId,
+                })
+
+            return ok(transactions)
+        } catch (error) {
+            console.error(error)
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse()
+            }
+
+            return serverError({
+                message: 'An error occurred while get the transaction',
+            })
+        }
+    }
 }
