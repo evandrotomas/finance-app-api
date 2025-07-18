f0e5648fd653a0403f38273cc6d95ad0bb5b2d17
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-18 20:57:51 -0300

    fix: pass correct params to  PostgresUpdateTransactionRepository

---
+++ src/controllers/transaction/update-transaction.js
@@ -0,0 +1,16 @@
+// import { serverError } from "../helpers"
+
+// export class UpdatetransactionController {
+//     constructor(updateTransactionUseCase) {
+//         this.updateTransactionUseCase = updateTransactionUseCase
+//     }
+
+//     async execute() {
+//         try {
+
+//         } catch (error) {
+//             console.error(error)
+//             return serverError()
+//         }
+//     }
+// }
