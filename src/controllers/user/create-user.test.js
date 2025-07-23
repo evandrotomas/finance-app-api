ee6ff5161ba27e7b03ce2d8a3b1f66c27d37bf68
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-23 20:09:54 -0300

    test: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -19,12 +19,33 @@ describe('Create User Controller', () => {
                 password: '697952',
             },
         }
-        // act
 
+        // act
         const result = await createUserController.execute(httpRequest)
 
         // assert
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
+    })
+
+    it('should returns 404 if first_name is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                // first_name: 'Evandro',
+                last_name: 'Tomas',
+                email: 'evandrotomas@gmail.com',
+                password: '697952',
+            },
+        }
+
+        // act
+        const result = await createUserController.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(400)
     })
 })
