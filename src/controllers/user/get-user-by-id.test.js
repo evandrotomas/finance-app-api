de5503207446a749f778e22dde367c3ad574da1c
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-25 16:22:48 -0300

    test: ensure GetUserByIdController return 500 if GetUserByIdUseCase throws

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -62,4 +62,18 @@ describe('GetUserByIdController', () => {
         // assert
         expect(result.statusCode).toBe(404)
     })
+
+    it('should return 500 if GetUserByIdController throws a error', async () => {
+        // arrange
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(500)
+    })
 })
