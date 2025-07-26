60847872729ed16965facb7ce1724e7fa586573c
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-25 21:23:01 -0300

    test: ensure CreateTransactionController returns 201 when creating a transaction successfully

---
+++ src/controllers/transaction/create-transaction.test.js
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker'
+import { CreateTransactionController } from './create-transaction'
+
+describe('CreateTransactionController', () => {
+    class CreateTransactionUseCaseStub {
+        async execute(transaction) {
+            return transaction
+        }
+    }
+
+    const makeSut = () => {
+        const createTransactionUseCase = new CreateTransactionUseCaseStub()
+        const sut = new CreateTransactionController(createTransactionUseCase)
+
+        return { sut, createTransactionUseCase }
+    }
+    const httpRequest = {
+        body: {
+            user_id: faker.string.uuid(),
+            name: faker.string.alphanumeric(7),
+            date: faker.date.anytime().toISOString(),
+            amount: Number(faker.finance.amount()),
+            type: 'EXPENSE',
+        },
+    }
+
+    it('should return 201 when creating a transaction successfully', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute(httpRequest)
+
+        // assert
+        expect(result.statusCode).toBe(201)
+    })
+})
