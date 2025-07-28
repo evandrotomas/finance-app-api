784e424fe30a1e8e14ed9e99dc6fd687c8b00fdf
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-28 19:42:36 -0300

    test: ensure UpdateTransactionController calls UpdateTransactionUseCase with correct params

--- src/controllers/transaction/updateTransaction.test.js
+++ src/controllers/transaction/updateTransaction.test.js
@@ -123,4 +123,18 @@ describe('UpdateTransactionController', () => {
         // Assert
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call UpdateTransactionUseCase with correct params', async () => {
+        // Arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        const executeSpy = jest.spyOn(updateTransactionUseCase, 'execute')
+
+        await sut.execute(httpRequest)
+
+        // Assert
+        expect(executeSpy).toHaveBeenCalledWith(
+            httpRequest.params.transactionId,
+            httpRequest.body,
+        )
+    })
 })
