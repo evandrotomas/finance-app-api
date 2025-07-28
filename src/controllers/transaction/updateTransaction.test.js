454b8814357fab622363ee30d47a224bfb2537e9
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-28 19:27:01 -0300

    test: ensure UpdateTransactionController returns 400 when amount is invalid

--- src/controllers/transaction/updateTransaction.test.js
+++ src/controllers/transaction/updateTransaction.test.js
@@ -75,4 +75,21 @@ describe('UpdateTransactionController', () => {
         // Assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when amount is invalid', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                amount: 'invalid_amount',
+            },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
 })
