cec3e038a695b9fcf3b653d5fcff764c2ac4e47d
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-28 19:24:42 -0300

    test: ensure UpdateTransactionController returns 400 when unallowed fields are provided

--- src/controllers/transaction/updateTransaction.test.js
+++ src/controllers/transaction/updateTransaction.test.js
@@ -58,4 +58,21 @@ describe('UpdateTransactionController', () => {
         // Assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when unallowed field is provided', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Act
+        const result = await sut.execute({
+            ...httpRequest,
+            body: {
+                ...httpRequest.body,
+                unallowed_field: 'some_value',
+            },
+        })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
 })
