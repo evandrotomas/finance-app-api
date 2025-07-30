f20eb9acec229e43788265d3da630fa204b2972f
Evandro Tomas <evandrotomas@hotmail.com>
2025-07-30 20:52:42 -0300

    fix: ensure passwordHasherAdapiter returns hashed password

--- src/adapters/password-hasher.js
+++ src/adapters/password-hasher.js
@@ -2,6 +2,6 @@ import bcrypt from 'bcrypt'
 
 export class PasswordHasherAdapter {
     async execute(password) {
-        await bcrypt.hash(password, 10)
+        return bcrypt.hash(password, 10)
     }
 }
