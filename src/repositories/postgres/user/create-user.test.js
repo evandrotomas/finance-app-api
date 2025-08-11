90fb1a287b698cee0f1896d1b3c507e51fc6857c
Evandro Tomas <evandrotomas@hotmail.com>
2025-08-11 20:46:38 -0300

    test: ensure PostgresCreateUserRepository thorws if Primas throws

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -23,4 +23,14 @@ describe('CreateUserRepository', () => {
 
         expect(prismaSpy).toHaveBeenCalledWith({ data: user })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresCreateUserRepository()
+
+        jest.spyOn(prisma.user, 'create').mockRejectedValueOnce(new Error())
+
+        const promisse = sut.execute(user)
+
+        await expect(promisse).rejects.toThrow()
+    })
 })
