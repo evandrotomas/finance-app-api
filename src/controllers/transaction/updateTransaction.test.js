af85ff115c96e58b7f0db10650330f79c0bf89fd
Evandro Tomas <evandrotomas@hotmail.com>
2025-08-18 19:28:55 -0300

    test: ensure UpdateTransactionController returns 404 when TransactionNotFoundError is thrown

--- src/controllers/transaction/updateTransaction.test.js
+++ src/controllers/transaction/updateTransaction.test.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 import { UpdatetransactionController } from './update-transaction'
 import { transaction } from '../../tests'
+import { TransactionNotFoundError } from '../../errors'
 
 describe('UpdateTransactionController', () => {
     class UpdateTransactionUseCaseStub {
@@ -116,6 +117,20 @@ describe('UpdateTransactionController', () => {
 
         // Assert
         expect(result.statusCode).toBe(500)
+    })
+
+    it('should return 404 when UserTUserNotFoundError is thrown', async () => {
+        // Arrange
+        const { sut, updateTransactionUseCase } = makeSut()
+        jest.spyOn(updateTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new TransactionNotFoundError(),
+        )
+
+        // Act
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(404)
     })
 
     it('should call UpdateTransactionUseCase with correct params', async () => {
