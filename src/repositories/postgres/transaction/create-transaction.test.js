60f28cd8a7c3f825ec93e9c0227d10da3e479cf5
Evandro Tomas <evandrotomas@hotmail.com>
2025-08-22 16:16:57 -0300

    test: fix test date in should create a transaction on db

--- src/repositories/postgres/transaction/create-transaction.test.js
+++ src/repositories/postgres/transaction/create-transaction.test.js
@@ -2,6 +2,9 @@ import { PostgresCreateTransactionRepository } from './create-transaction'
 import { transaction, user } from '../../../tests'
 import { prisma } from '../../../../prisma/prisma'
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
 
 describe('PostgresCreateTransactionRepository', () => {
     it('should create a transaction on db', async () => {
@@ -11,15 +14,16 @@ describe('PostgresCreateTransactionRepository', () => {
 
         const result = await sut.execute({ ...transaction, user_id: user.id })
 
+        const resultDate = dayjs(result.date).utc().startOf('day')
+        const expectedDate = dayjs(transaction.date).utc().startOf('day')
+
         expect(result.name).toBe(transaction.name)
         expect(result.type).toBe(transaction.type)
         expect(result.user_id).toBe(user.id)
         expect(String(result.amount)).toBe(String(transaction.amount))
-        expect(dayjs(result.date).daysInMonth()).toBe(
-            dayjs(transaction.date).daysInMonth(),
-        )
-        expect(dayjs(result.date).month()).toBe(dayjs(transaction.date).month())
-        expect(dayjs(result.date).year()).toBe(dayjs(transaction.date).year())
+        expect(resultDate.date()).toBe(expectedDate.date())
+        expect(resultDate.month()).toBe(expectedDate.month())
+        expect(resultDate.year()).toBe(expectedDate.year())
     })
 
     it('should call Prisma with correct params', async () => {
