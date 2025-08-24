6a3e0c6a037e45f1e79c6a00dcb88380e5820a06
Evandro Tomas <evandrotomas@hotmail.com>
2025-08-24 10:52:50 -0300

    feat: add LoginUserController

---
+++ src/controllers/user/login-user.js
@@ -0,0 +1,45 @@
+import { ZodError } from 'zod'
+import { loginSchema } from '../../schemas/user.js'
+import {
+    badRequest,
+    notFound,
+    ok,
+    serverError,
+    unauthorized,
+} from '../helpers/index.js'
+import { InvalidPasswordError, UserNotFoundError } from '../../errors'
+
+export class LoginUserController {
+    constructor(loginUseUserCase) {
+        this.loginUseUserCase = loginUseUserCase
+    }
+    async execute(httpRequest) {
+        try {
+            const params = httpRequest.body
+            await loginSchema.parseAsync(params)
+            const user = await this.loginUseUserCase.execute(
+                params.email,
+                params.password,
+            )
+            return ok(user)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues[0].message,
+                })
+            }
+
+            if (error instanceof InvalidPasswordError) {
+                return unauthorized()
+            }
+
+            if (error instanceof UserNotFoundError) {
+                return notFound({
+                    message: 'User not found',
+                })
+            }
+
+            return serverError()
+        }
+    }
+}
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -13,7 +13,7 @@ export const createUserSchema = z.object({
         .email({ message: 'Please provide a valid e-mail.' })
         .trim()
         .min(1, { message: 'E-mail  is required.' }),
-    password: z.string({ message: 'Password is required.' }).trim().min(6, {
+    password: z.string().trim().min(6, {
         message: 'Password must have at least 6 characters.',
     }),
 })
@@ -21,3 +21,14 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema
     .partial()
     .strict({ message: 'Some provided field is not allowed.' })
+
+export const loginSchema = z.object({
+    email: z
+        .email({ message: 'Please provide a valid e-mail' })
+        .trim()
+        .min(1, { message: 'E-mail is required' }),
+    password: z
+        .string()
+        .trim()
+        .min(6, { message: 'Password must have at least 6 characters.' }),
+})
